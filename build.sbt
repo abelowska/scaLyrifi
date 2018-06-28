9acf9262a6b3277c1fae69ffba8ef5feb063f160
Anna <anna.belowska2@gmail.com>
2018-06-28 03:43:39 +0200

    Generating pdf file from list of songs

--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
 name := "scalyrifi"
 version := "0.1"
 scalaVersion := "2.12.6"
-libraryDependencies += "net.sourceforge.tess4j" % "tess4j" % "3.4.9"
+libraryDependencies += "net.sourceforge.tess4j" % "tess4j" % "3.4.9" exclude("net.java.dev.jna", "jna")
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.5.13",
   "com.typesafe.akka" %% "akka-testkit" % "2.5.13" % Test)
@@ -9,6 +9,7 @@ libraryDependencies ++= Seq(
 libraryDependencies += "org.seleniumhq.selenium" % "selenium-java" % "2.8.0"
 libraryDependencies += "com.machinepublishers" % "jbrowserdriver" % "0.17.11"
 libraryDependencies += "org.jsoup" % "jsoup" % "1.11.3"
+libraryDependencies += "org.apache.pdfbox" % "pdfbox" % "2.0.10"
 
 
 
--- src/main/scala/services/PdfCreator.scala
+++ src/main/scala/services/PdfCreator.scala
@@ -1,45 +1,75 @@
 package services
 
-import java.io.FileOutputStream
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, InputStream}
 
 import Data.Song
 import com.lowagie.text._
-import com.lowagie.text.pdf.PdfWriter
+import com.lowagie.text.pdf._
+import org.apache.pdfbox.io.MemoryUsageSetting
+import org.apache.pdfbox.multipdf.PDFMergerUtility
+
+import scala.collection.JavaConverters._
+
+object PdfCreator {
+
+  private val TitleFont = {
+    val tf = new Font
+    tf.setStyle(Font.BOLD)
+    tf.setSize(7)
+    tf
+  }
+
+  private val AuthorFont = {
+    val af = new Font
+    af.setStyle(Font.BOLD)
+    af.setSize(6)
+    af
+  }
+
+  private val BasicFont = {
+    val bf = new Font
+    bf.setStyle(Font.NORMAL)
+    bf.setSize(5)
+    bf
+  }
+
+}
 
 class PdfCreator(fileName: String) {
 
-  def saveOne(song: Song): Unit = {
+  import PdfCreator._
+
+  def save(songs: Seq[Song]): Unit = {
+
+    val inputStreams = songs.map(s => generateOne(s))
+
+    val util = new PDFMergerUtility
+    util.addSources(inputStreams.asJava)
+    util.setDestinationFileName(s"./$fileName.pdf")
+
+    util.mergeDocuments(MemoryUsageSetting.setupMainMemoryOnly())
+
+  }
+
+  def generateOne(song: Song): InputStream = {
+    val out = new ByteArrayOutputStream
     val pdfDoc = new Document(PageSize.A4)
-    PdfWriter.getInstance(pdfDoc, new FileOutputStream(s"$fileName.pdf")).setPdfVersion(PdfWriter.PDF_VERSION_1_7)
+    PdfWriter.getInstance(pdfDoc, out).setPdfVersion(PdfWriter.PDF_VERSION_1_7)
     pdfDoc.open()
 
-    val titleFont = new Font
-    titleFont.setStyle(Font.BOLD)
-    titleFont.setSize(7)
-
-    val authorFont = new Font
-    authorFont.setStyle(Font.BOLD)
-    authorFont.setSize(6)
-
-    val myfont = new Font
-    myfont.setStyle(Font.NORMAL)
-    myfont.setSize(5)
-
-    val title = new Paragraph(song.header.title + "\n", titleFont)
-    val author = new Paragraph(song.header.author + "\n", authorFont)
+    val title = new Paragraph(song.header.title + "\n", TitleFont)
+    val author = new Paragraph(song.header.author + "\n", AuthorFont)
 
     pdfDoc.add(title)
     pdfDoc.add(author)
 
-    val para = new Paragraph(song + "\n", myfont)
+    val para = new Paragraph(song + "\n", BasicFont)
     para.setAlignment(Element.ALIGN_JUSTIFIED)
     pdfDoc.add(para)
 
     pdfDoc.close()
-  }
 
-  def saveMany(songs: Seq[Song]): Unit = {
-
+    new ByteArrayInputStream(out.toByteArray)
   }
 
 
